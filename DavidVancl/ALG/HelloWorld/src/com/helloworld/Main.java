af48c25530aea93c5721502047bba79dc6bb0418
vancl <vancldada@gmail.com>
2020-10-02 11:03:20 +0200

    HelloWorld project, ALD

---
+++ DavidVancl/ALG/HelloWorld/src/com/helloworld/Main.java
@@ -0,0 +1,12 @@
+package com.helloworld;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int count = new Scanner(System.in).nextInt();
+        for(int i = 0; i < count;i++){
+            System.out.println("Hello world!");
+        }
+    }
+}
