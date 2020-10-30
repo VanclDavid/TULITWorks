dbdb6f3a1540008da5985fd934e6b9a6566d4ef8
unknown <david.dlouhy@tul.cz>
2020-10-30 23:48:35 +0100

    ALD úlohy + miny

---
+++ DavidDlouhy/ALD/Java/cviceni1/main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if (n>=0)
+        {
+            for (int i=0; i<n; i++)
+            {
+                System.out.println("Hello world!");
+            }
+        }
+    }
+}
---
+++ DavidDlouhy/ALD/Pyton/main.py
@@ -0,0 +1,10 @@
+class Node:
+    #value
+    #left
+    #right
+    def __init__(self, n):
+        self.value=n
+        self.left, self.right = None, None
+uzel=Node(5)
+uzel.lef = Noed(6)
+print(uzel)
