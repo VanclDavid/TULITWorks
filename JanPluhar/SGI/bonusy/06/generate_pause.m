24a6b9f50a5b52af220bcd9c8ccd15b1f6c6e271
jan.pluhar <jan.pluhar@tul.cz>
2020-11-07 17:12:38 +0100

    sgi bonus 06

---
+++ JanPluhar/SGI/bonusy/06/generate_pause.m
@@ -0,0 +1,10 @@
+function [sound] = generate_pause(Fs,isNoise)
+t=round(rand*50)+100;
+if(isNoise)
+    sound=zeros(1,t*Fs/1000);
+    sound=sound+(rand(1,length(sound))*0.06);
+else 
+    sound=zeros(1,t*Fs/1000);
+end
+end
+
