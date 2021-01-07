779a417ee8194dadb18f1b550648b3fd4d5c771a
DanielKnourek <dev.danielknourek@gmail.com>
2021-01-07 21:53:22 +0100

    Hospital numbers

--- DBS semestralka/queries.sql
+++ DBS semestralka/queries.sql
@@ -29,3 +29,16 @@ SELECT [Pharmacies].[name], [Medicaments].[name], [Medicaments].[type], [Stocks]
     LEFT JOIN Medicaments 
         ON Medicaments.id = Stocks.id_medicaments
 WHERE Pharmacies.id = 2
+
+-- How many doctors does hospital have and how many recipes on doctor have been created
+ SELECT Hospitals.id ,Hospitals.name , ISNULL(doctor_count,0) AS 'doctor count', ISNULL(recipe_count,0) AS 'recipe count', ISNULL((recipe_count / doctor_count),0) AS 'Recipies on doctor'
+ 
+ FROM Hospitals
+ LEFT JOIN (SELECT id_hospital, sum(var_recipe_count) AS 'recipe_count' FROM Doctors JOIN 
+		(SELECT id_doctor, count(id_doctor) AS 'var_recipe_count' FROM Recipes GROUP BY (id_doctor) ) AS R 
+        ON R.id_doctor = Doctors.id GROUP BY id_hospital) AS T1	
+    ON T1.id_hospital = Hospitals.id
+ LEFT JOIN 
+        (SELECT id_hospital, count(id_hospital) AS 'doctor_count' FROM Doctors GROUP BY id_hospital) AS T2
+    ON T2.id_hospital = Hospitals.id 
+ 
